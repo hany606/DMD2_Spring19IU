8116ab8a1e112c1399a0b21b31124b6b7efba021
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-03-20 18:07:49 +0300

    Finish Query 1

--- Assignments/Assignment1/src/Queries.py
+++ Assignments/Assignment1/src/Queries.py
@@ -7,15 +7,18 @@
 # (3). https://stackoverflow.com/questions/26984799/find-duplicate-records-in-mongodb
 # (4). https://dzone.com/articles/basic-aggregation-mongodb-21
 # (5). https://www.compose.com/articles/aggregations-in-mongodb-by-example/
+# (6). https://www.isummation.com/blog/perform-inner-join-in-mongodb-using-lookup-aggregation-operator/
 # ---------------------------------------------------------------------------------------------------
 
 from utils import *
 from datetime import datetime
+import pprint
 
 
 class Queries:
     def __init__(self,db):
         self.db = db
+        self.pp = pprint.PrettyPrinter()
 
     def query(self, q_num, params=None):
         queries = [self._query1, self._query2, self._query3, self._query4, self._query5]
@@ -42,6 +45,15 @@ class Queries:
         to_dt = datetime.strptime('{:}-12-31'.format(current_year),'%Y-%m-%d')
         my_pipeline = [
                         {
+                            "$lookup":
+                            {
+                            "from": "customer",
+                            "localField": "customer_id",
+                            "foreignField": "customer_id",
+                            "as": "customer"
+                            }
+                        },
+                        {
                             "$match": 
                             {
                                 "rental_date": 
@@ -51,10 +63,12 @@ class Queries:
                                 }
                             } 
                         }, 
+                        {"$project": {"customer" : "$customer"} }, 
                         {
                             "$group" : 
                             {
-                                "_id": "$customer_id",
+                                # "_id": "$customer.customer_id",   # For getting only the customer id not the whole info
+                                "_id": "$customer",
                                 "count": { "$sum": 1 } 
                             } 
                         },
@@ -65,14 +79,16 @@ class Queries:
                             } 
                         }, 
                         {"$sort": {"count" : -1} },
-                        {"$project": {"customer_id" : "$_id", "num_times":"$count", "_id" : 0} }    
+                        {"$project": {"customer":"$_id", "num_rented_movies_in_the_current_year":"$count", "_id" : 0} }    
         ]
         # t = self.db.rental.find(my_query,my_fields)
         a = self.db.rental.aggregate(my_pipeline)
         results = []
         for i in a:
             results.append(i)
-            print(i)
+            print("---------------------------------")
+            self.pp.pprint(i)
+            print("---------------------------------")
         print(len(results))
         
     def _query2(self,params=None):
